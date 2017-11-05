e286952c8f406de448dc405625bc0677c2b88ff6
Daniel Guzman <danielucho512@hotmail.com>
2017-11-05 13:51:06 -0500

    Inicial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	platzimusic
+// API key	588dc20567d0ae0f57581d8f5036e681
+// Shared secret	01bd5c618a55ddf3d950563803306f8c
+// Registered to	holaskay
+
+export default {
+  appName: 'platzimusic',
+  apiKey: '588dc20567d0ae0f57581d8f5036e681',
+  secret: '01bd5c618a55ddf3d950563803306f8c',
+  registeredTo: 'holaskay'
+}
